a0a5c19557b80e328d344a3ed0d679ae7d6d560f
yan.zhao <catcatcarrot1207@163.com>
2020-08-05 13:41:54 +0800

    start springboot

---
+++ src/main/java/com/zy/library/TwLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.zy.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwLibraryApplication.class, args);
+	}
+
+}
